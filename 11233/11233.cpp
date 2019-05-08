e69733ee03e1c2b4a202abb1b58995e3cbf40044
guoxiaofeng <1136908271@qq.com>
2019-05-08 16:41:09 +0800

    774411674

--- 11233/11233.cpp
+++ 11233/11233.cpp
@@ -4,14 +4,13 @@ void setup() {
 int aaa=0;
 int dfdf=0;
 int dfjdjj=0;
+int dfas=0;
 }
 
 void loop() {
 while(1);
-<<<<<<< HEAD
 while(5);
 sprintf("jjsjdjjd");
-=======
 printf("hellowow");
->>>>>>> refs/heads/new
+
 }
